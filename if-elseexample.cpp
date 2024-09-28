196932b3227e21d45c906657f745b2472ed93e49
anushajindal2046 <157337723+anushajindal2046@users.noreply.github.com>
2024-09-28 21:04:21 +0530

    Add files via upload

---
+++ if-elseexample.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+// int main(){
+//     int age;
+//     cout<<"Enter the age : ";
+//     cin>>age;
+
+//     if(age >=18){
+//         cout<<"can vote"<<endl;
+//     }else{
+//         cout<<"Can not vote"<<endl;
+//     }
+//     return 0;
+// }
+
+// int main(){
+//     int age;
+//     cout<<"Enter the age : ";
+//     cin>>age;
+
+//     if(age >=18){
+//         cout<<"can vote"<<endl;
+//     }
+//     if(age >=45){
+//         cout << "contest for elections"<<endl;
+//     }
+//     else{
+//         cout<<"Can not vote"<<endl;
+//     }
+//     return 0;
+// }
+
+int main(){
+    float marks;
+    cout<<"Enter the marks : ";
+    cin>> marks;
+
+    if (marks >= 33){
+        cout << "Pass"<<endl;
+    }
+    else{
+        cout<<"Fail"<<endl;
+    }
+}
+
