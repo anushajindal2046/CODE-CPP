3e247b70775f998003fcc2ed849886aef4554212
anushajindal2046 <157337723+anushajindal2046@users.noreply.github.com>
2024-10-01 22:35:41 +0530

    Add files via upload

---
+++ ternaryoperator.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+// // IS ADULT OR NOT 
+// int main() {
+//     bool isAdult; //1, 0 (true or false)
+//     int age;
+//     cout << "Enter age : ";
+//     cin >> age;
+
+//     // if (age >= 18) {
+//     //     isAdult = true;
+//     // } else {
+//     //     isAdult = false;
+//     // }
+
+//     isAdult = age >= 18 ? true : false;
+//     return 0;
+// }
+
+// //Largest of 2 numbers
+// int main() {
+//     int a = 10;
+//     int b = 5;
+
+//     int largest = a>=b ? a : b;
+//     cout<<"largest is "<<largest<<endl;
+//     return 0;
+// }
+
+// Odd or Even 
+int main()
+{
+    int num = 16;
+    bool isEven = (num % 2 == 0) ? true : false;
+    cout<<isEven<<endl;
+    return 0;
+
+}
